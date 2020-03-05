5bcfce35400d186378652b4f58f979fd2d88f2bb
Justin Montgomery <montgomerycodepro@gmail.com>
2020-03-05 10:05:53 -0500

    delete button working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3005 
 let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -22,13 +23,15 @@ app.get("/api/notes",  (req, res) => {
         if(err) {
             throw err
         } 
-        res.json(JSON.parse(data)) 
+        res.json(JSON.parse(data))
     })
 })
 
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
-    let id = savedNote.length.toString();
+    let id;
+    if (!savedNote.length) id = 0
+    else id = savedNote[savedNote.length-1].id +1
     newNote.id = id
     savedNote.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNote));
@@ -36,12 +39,13 @@ app.post("/api/notes", (req, res) => {
 })
 
 app.delete("/api/notes/:id", (req, res) => {
-//    let note = req.body.id
-//    savedNote = savedNote.filter(element=> {
-//        return element.id != note
-//    })
-   fs.writeFileSync("./db/db.json", JSON.stringify(savedNote));
-   res.json(savedNote);
+    let id = req.params.id
+    const index = savedNote.findIndex(note=> note.id === parseInt(id))
+    savedNote.splice(index, 1)
+   fs.writeFile("./db/db.json", JSON.stringify(savedNote), err=>{
+       if(err) throw err
+       res.json(savedNote);
+   });
 } )
 
 
